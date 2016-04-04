e07a85c606bf5918446e013ee8c15f5506ce05f8
Lucas Biagini <lbs4@cin.ufpe.br>
2016-04-04 15:44:45 -0400

    installing bootstrap

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -2,4 +2,36 @@ class AdsController < ApplicationController
 	def index
 		@ads = Ad.all
 	end
+
+	def show
+		redirect_to ads_path
+	end
+
+	def new
+		@ad = Ad.new
+	end
+
+	def edit
+	end
+
+	def create
+		@ad = Ad.new(ad_params)
+
+		if @ad.save
+			redirect_to @ad
+		else
+			render 'new'
+		end
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+		def ad_params
+			params.require(:ad).permit(:budget)
+		end
 end
