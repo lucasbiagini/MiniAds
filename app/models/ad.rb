467d520011592fce950acb1ce025813dd1234fbf
Lucas Biagini <lbs4@cin.ufpe.br>
2016-04-04 15:57:30 -0400

    added validations to ad.budget

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,8 @@
 class Ad < ActiveRecord::Base
+
+	validates	:budget, presence: true
+	validates	:budget, :numericality => {:greater_than => 0}
+
 	def change
 		create_table :ads do |t|
 			t.float :budget
