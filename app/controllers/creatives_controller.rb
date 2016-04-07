bca4b1f6f70773de5d484140cf0d3b69452a68b4
Lucas Biagini <lbs4@cin.ufpe.br>
2016-04-06 22:55:59 -0400

    installed gem cocoon

--- app/controllers/creatives_controller.rb
+++ app/controllers/creatives_controller.rb
@@ -1,2 +1,13 @@
 class CreativesController < ApplicationController
+
+	def create
+		@ad = ad.find(params[:ad_id])
+		@creative = @ad.creatives.create(creative_params)
+		redirect_to ad_path(@ad)
+	end
+
+	private
+		def creative_params
+			params.require(:creatives).permit(:bid, :adtext)
+		end
 end
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,8 +1,13 @@
 class Ad < ActiveRecord::Base
 	has_many :creatives
-	
+
 	validates	:budget, presence: true
 	validates	:budget, :numericality => {:greater_than => 0}
+	validates	:creatives, presence: true
+
+	accepts_nested_attributes_for :creatives,
+		:allow_destroy => true,
+		:reject_if => :all_blank
 
 	def change
 		create_table :ads do |t|
