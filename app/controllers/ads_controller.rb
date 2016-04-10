b7b477848a74da0b83e68e5d04fbe75a92367988
Lucas Biagini <lbs4@cin.ufpe.br>
2016-04-10 00:04:46 -0400

    UI redesign

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -22,7 +22,7 @@ class AdsController < ApplicationController
 		@ad = Ad.new(ad_params)
 
 		if @ad.save
-			redirect_to @ad
+			redirect_to ads_path
 		else
 			render 'new'
 		end
@@ -31,17 +31,21 @@ class AdsController < ApplicationController
 	def update
 		@ad = Ad.find(params[:id])
 		if @ad.update(ad_params)
-			redirect_to @ad
+			redirect_to ads_path
 		else
 			render 'edit'
 		end
 	end
 
 	def destroy
+		@ad = Ad.find(params[:id])
+		@ad.destroy
+
+		redirect_to ads_path
 	end
 
 	private
 		def ad_params
-			params.require(:ad).permit(:budget, creatives_attributes: [:id, :bid, :adtext, :_destroy], targetings_attributes: [:id, :gender, :_destroy, places_attributes: [:id, :name]])
+			params.require(:ad).permit(:budget, creatives_attributes: [:id, :bid, :adtext, :_destroy], targetings_attributes: [:id, :gender, :_destroy, places_attributes: [:id, :name, :_destroy]])
 		end
 end
